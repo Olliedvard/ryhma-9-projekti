1a4e7d2ff3e66da4758f33c3d9fe105d59640ea9
nicopasma <n3pani00@students.oamk.fi>
2024-05-01 02:55:07 +0300

    Tulossivua luonnosteltu, navbarin nappeihin tehty linkit muihin sivuihin.

---
+++ js/tulokset.js
@@ -0,0 +1,6 @@
+addEventListener("DOMContentLoaded", () => {
+    parhaatnico = Number(sessionStorage.getItem("parhaatprosenttipisteet")) //Haetaan prosenttipelin js-koodissa määritelty muuttuja "parhaatprosenttipisteet"
+    document.querySelector('#parhaatnico').innerHTML = parhaatnico //Näytetään "parhaatprosenttipisteet" prosenttipelin "parhaat pisteet" -sarakkeella.
+
+
+})
