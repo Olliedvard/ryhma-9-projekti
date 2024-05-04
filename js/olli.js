0a5ae1af0a0ed75806099dd244050ff0b202acce
Olliedvard <olli.nuorala@gmail.com>
2024-05-04 22:46:55 +0300

    lähes valmis

--- js/olli.js
+++ js/olli.js
@@ -1,30 +1,32 @@
+const input = document.getElementById("inputti")
 
-let rand_num1 = 0
-let rand_num2 = 0
+let rand_num = 0
+let num2 = 2
 
+oikein = 0
+vaarin = 0
 
 const getRandomIntNumberInRange = (min,max) => {
     return Math.floor(Math.random() * max) + min;
 }
 
 const randomizeNumbers = () => {
-    rand_num1 = getRandomIntNumberInRange(1,10)
-    rand_num2 = getRandomIntNumberInRange(1,10)
-    console.log(rand_num1,' * ', rand_num2,' = ' ,rand_num1*rand_num2)
+    rand_num = getRandomIntNumberInRange(1,10)
+    
+    console.log(rand_num,' ** ', num2,' = ' ,rand_num**num2)
 
-    document.querySelector('#num1').innerHTML = rand_num1
-    document.querySelector('#num2').innerHTML = rand_num2*10
+    document.querySelector('#num1').innerHTML = rand_num
+    document.querySelector('#num2').innerHTML = num2
 }
 
 addEventListener("DOMContentLoaded", () => {
     randomizeNumbers()
-    document.querySelector('#oikein').innerHTML = oikein
-    document.querySelector('#vaarin').innerHTML = vaarin
 })
 
-document.querySelector('button').addEventListener('click',() => {
+document.querySelector('#vastaus').addEventListener('click',() => {
     const answer = Number(document.querySelector('input').value)
-    const correctAnswer = rand_num1 * rand_num2*10
+    const correctAnswer = rand_num**num2
+    console.log(correctAnswer)
     if (answer === correctAnswer) {
         alert('Oikein!')
         oikein++
@@ -33,6 +35,9 @@ document.querySelector('button').addEventListener('click',() => {
         vaarin++
     }
 
-    randomizeNumbers()
-    document.querySelector('input').value=''
+    
+
+console.log(input.value)
+input.value=""
+randomizeNumbers()
 })
