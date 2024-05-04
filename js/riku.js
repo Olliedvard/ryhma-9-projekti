38e3ec8e1c73f075308e2d3e4bfdfe19c3c70a5b
Riku Jääskeläinen <jaaskelainen.riku10@gmail.com>
2024-05-04 23:23:18 +0300

    Pelin koodi toimii

--- js/riku.js
+++ js/riku.js
@@ -1,27 +1,55 @@
 let rand_num1 = 0
+let pisteet = 0
+let kysymysnro = 0
+let satunnainen = 0
+const nappi = document.getElementById('laske')
+const virhev = document.getElementById('virheviesti')
 
+//Satunnaisen numeron luonti koodi
 const getRandomIntNumberInRange = (min,max) =>{
     return Math.floor(Math.random() * max) + min;
 }
 
+//Luo satunnaisen numeron 1-12 ja kertoo sen itsellään
 const randomizeNumbers = () =>{
     rand_num1 = getRandomIntNumberInRange(1,12)**2
     document.querySelector('#num1').innerHTML = rand_num1
+}
+
+const peliOhi = () => {
+    alert("Onneksi olkoon!. Sait "+[pisteet]+"pistettä.")
+    nappi.remove()
+    clearInterval(ajastinON);
+    if (pisteet > paras) {
+        paras = pisteet
+        sessionStorage.setItem("parhaatprosenttipisteet", paras.toString())
+        document.querySelector('#ennätys').innerHTML = paras
+    }
 }
 
 addEventListener("DOMContentLoaded", () =>{
     randomizeNumbers()
 });
 
-document.querySelector('button').addEventListener('click', () =>{
-    const answer = Number(document.querySelector('input').value)
-    const correctAnswer = Math.sqrt(rand_num1)
-    if (answer === correctAnswer){
-        alert('Correct!')
-    }   else {
-        alert('Incorrect!')
-    }
+    //User Input Check
+    nappi.addEventListener("click", () => {
+        virhev.classList.add("hide");
+        let userInput = Number(document.getElementById("vastaus").value);
+        let answerValue = Math.sqrt(rand_num1)
+        //If user input is not empty
+        if (userInput) {
+          if (userInput == answerValue) { //Käyttäjä vastaa oikein
+            alert('Oikein!');
+          }
+          else { //Käyttäjä vastaa väärin
+            alert('Väärin!');
+          }
+        }
+        else { //Käyttäjän vastaus on tyhjä
+          virhev.classList.remove("hide");
+          virhev.innerHTML = "Laita vastaus vastauskenttään!";
+        }
+      });
 
     randomizeNumbers()
     document.querySelector('input').value=''
-})
